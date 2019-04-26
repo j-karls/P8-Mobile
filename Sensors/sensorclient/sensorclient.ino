c823bb65e121061fb355f0306208237d80bf0d2d
Hem <mikkel@webhem.dk>
2019-04-26 13:47:23 +0200

    Finished MQ7: cool/heat cycles, ppm conversion

--- Sensors/sensorclient/sensorclient.ino
+++ Sensors/sensorclient/sensorclient.ino
@@ -25,19 +25,18 @@ class SensorCO : public Sensor {
     int _pinDigital;
 
     bool _isHeating = false;
-    unsigned long _heatPeriod = 6000;
-    unsigned long _coolPeriod = 9000; //change it back REEEEEEEEEEEEEEEEEE
+    unsigned long _heatPeriod = 60000;
+    unsigned long _coolPeriod = 90000; 
     int _numMeasurements = 0;
     double _aggregatedValue = 0;
 
-    int _analogMeasurementFresh = 30;
+    double _analogMeasurementFresh = 40;
     double _rRatioFresh = 1; // r-ratio constant in fresh air as defined by datasheet
-    double _r0 = _rRatioFresh * (convertRS(_analogMeasurementFresh)); // a constant that is determined based on sensor results in clean air
-    double _funcConstantA = 18.5428; // a constant that is determined by finding the regression of the curve describing sensor output in the datasheet 
-    double _funcConstantB = -1.0519;  // another constant from the same regression
+    double _r0 = -1; // a constant that is determined based on sensor results in clean air
+    double _funcConstantA = 0.8494; // a constant that is determined by finding the regression of the curve describing sensor output in the datasheet 
+    double _funcConstantB = -0.5455;  // another constant from the same regression
     double _rl = 10000; // load resistance in Ohm
     double _vc = 5; // control voltage in Volt
-
 
     void startNewCycle(unsigned long newCycleStartTime) {
       _cycleStartTime = newCycleStartTime;
@@ -49,16 +48,22 @@ class SensorCO : public Sensor {
     }
     double convertVRL(double analogValue) {
       // analogValue is returned by the reading the sensor with analogRead and is a number between 0 and 1023
-      return analogValue / 1023; // voltage over the load resistor in Volt: basically, the "output" voltage of the circuit
+      return (analogValue / 1023) * 5; // voltage over the load resistor in Volt: basically, the "output" voltage of the circuit, multiplied with 5, as it is the maximum voltage that can be returned by the arduino
     }
     double convertRS(double analogValue) {
-      double vrl = convertVRL(analogValue);
+      double vrl = convertVRL(analogValue);  
       return ((_vc * _rl) / vrl) - _rl; // rs is resistance over the sensor in Ohm
     }
     double convertPPM(double analogValue) {
       double rs = convertRS(analogValue);
       double rRatio = rs / _r0; 
-      return pow((_funcConstantA / rRatio), (1 / _funcConstantB)); // A power function of the form PPM(RRatio) = (a / RRatio) ^ (1 / b)
+      
+      // Serial.println(String(analogValue, 4));
+      // Serial.println(String(_r0, 4));
+      // Serial.println(String(rRatio, 4));
+      Serial.println(String((pow((_funcConstantA / rRatio), (1 / -_funcConstantB))), 4));
+      
+      return pow((_funcConstantA / rRatio), (1 / -_funcConstantB)); // A power function of the form PPM(RRatio) = (a / RRatio) ^ (1 / b)
     }
   public:
     String name = String("CO");
@@ -76,6 +81,7 @@ class SensorCO : public Sensor {
         canGetNewValue = false;
         cycleTime = _heatPeriod + _coolPeriod;
         _cycleStartTime = 0;
+        _r0 = _rRatioFresh * (convertRS(_analogMeasurementFresh));
     }
     double getRawValue() {
       return analogRead(_pinData);
@@ -104,9 +110,12 @@ class SensorCO : public Sensor {
       else if (cycleProgress >= _heatPeriod && _isHeating) { // switch state to cooling
         cool();
       }
-      else { // if we're in the cooling state
+      else if (cycleProgress >= _heatPeriod) { // if we're in the cooling state
+        heat(); // turn on the sensor for short time, such that we can actually read values
         performAggregation(convertPPM(getRawValue()));
+        cool(); // return to cooling state
       }
+      // otherwise, we´re currently heating, and should continue to do so
     }
     double getValue() {
       canGetNewValue = false;
@@ -173,9 +182,10 @@ void loop()
 
   // Get new value
   if (sensorCO.canGetNewValue) {
-    Serial.println(currentTime);
+    //Serial.println(currentTime);
     Serial.println(String(sensorCO.name + String('|') + String(sensorCO.getValue(), 4)));
-    Serial.println(sensorCO.getRawValue());
+    //Serial.println(sensorCO.getRawValue());
+    Serial.println("------------------------");
   }
   
   // Manage state
